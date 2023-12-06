0389961429c718af33330d1032b18a7a3e4a147d
Neo40 <tron36.cc@gmail.com>
2023-12-06 17:01:37 -0300

    Geração de getters and setters

--- src/Endereco.java
+++ src/Endereco.java
@@ -3,4 +3,27 @@ public class Endereco {
     private String complemento;
     private String bairro;
 
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 }
