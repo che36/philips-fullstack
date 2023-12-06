4c4d943ef9f931c0250dfa4e81e873b89a9d33e1
Neo40 <tron36.cc@gmail.com>
2023-12-06 17:39:44 -0300

    Geração de setter and getters

--- src/FuncionarioCLT.java
+++ src/FuncionarioCLT.java
@@ -13,4 +13,36 @@ public class FuncionarioCLT {
 
     public FuncionarioCLT() {
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Double getValorSalario() {
+        return valorSalario;
+    }
+
+    public void setValorSalario(Double valorSalario) {
+        this.valorSalario = valorSalario;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
