1e0db9990e80b44bfb1a95fd68d700071ac15983
Neo40 <tron36.cc@gmail.com>
2023-12-06 17:59:52 -0300

    Criação de 2 objetos

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Endereco endereco = new Endereco("Rua funcionario", "Complemento Funcionario", "Bairro Funcionario");
+
+        Vendedor vendedor = new Vendedor();
+        vendedor.setNome("Cesar Constanzo");
+        vendedor.setDocumento("123456467");
+        vendedor.setValorSalario(1000d);
+        vendedor.setEndereco(endereco);
+        System.out.println(vendedor);
     }
 }
