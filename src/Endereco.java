b38ab8bf3dbdbe10f8426acd72cd0cd67c8ec698
Neo40 <tron36.cc@gmail.com>
2023-12-06 17:03:05 -0300

    Geração de 2 construtores

--- src/Endereco.java
+++ src/Endereco.java
@@ -3,6 +3,15 @@ public class Endereco {
     private String complemento;
     private String bairro;
 
+    public Endereco(String rua, String complemento, String bairro) {
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+    }
+
+    public Endereco() {
+    }
+
     public String getRua() {
         return rua;
     }
