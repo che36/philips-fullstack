94d43e848a8b31eaed8b1ddc85af6e4632a100be
Neo40 <tron36.cc@gmail.com>
2023-12-06 17:37:46 -0300

    Adição de 2 contrutores da classe

--- src/FuncionarioCLT.java
+++ src/FuncionarioCLT.java
@@ -3,4 +3,14 @@ public class FuncionarioCLT {
     protected String documento;
     protected Double valorSalario;
     protected Endereco endereco;
+
+    public FuncionarioCLT(String nome, String documento, Double valorSalario, Endereco endereco) {
+        this.nome = nome;
+        this.documento = documento;
+        this.valorSalario = valorSalario;
+        this.endereco = endereco;
+    }
+
+    public FuncionarioCLT() {
+    }
 }
