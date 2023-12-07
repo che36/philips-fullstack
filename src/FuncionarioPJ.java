593b96a0824255c0fc5964c45b244cd3193f37be
Neo40 <tron36.cc@gmail.com>
2023-12-07 07:06:05 -0300

    Criação de classse com seus atributos

---
+++ src/FuncionarioPJ.java
@@ -0,0 +1,8 @@
+public abstract class FuncionarioPJ {
+    private String nome;
+    private String documento;
+    private Endereco endereco;
+    private Integer horasTrabalhadas;
+    private Double valorHora;
+    private Double valorRemuneracao;
+}
