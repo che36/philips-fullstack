c78fe6bd7b76c486d17168d11718e9cfa28dc18a
Neo40 <tron36.cc@gmail.com>
2023-12-07 07:50:21 -0300

    Sobrescrita de método de interface

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,10 +1,15 @@
-public class Gerente extends FuncionarioPJ {
-    private Double valorBonidficacao;
+public class Gerente extends FuncionarioPJ implements CalculaBonificacao {
+    private Double valorBonificacao;
+
+    @Override
+    public void calculaBonificacao(Double porcentagemBonificacao) {
+        this.valorBonificacao = (super.getValorRemuneracao() * (porcentagemBonificacao / 100)) + 100d;
+    }
 
     @Override
     public String toString() {
         return "Gerente{" +
-                "valorBonidficacao=" + valorBonidficacao +
+                "valorBonidficacao=" + valorBonificacao +
                 "nome='" + super.getNome() + '\'' +
                 ", documento='" + super.getEndereco().getBairro() + '\'' +
                 ", endereco=" + super.getEndereco() +
