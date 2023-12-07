6ec4be176dcbf299e48b00640a0f69807de8c67a
Neo40 <tron36.cc@gmail.com>
2023-12-07 07:25:43 -0300

    Geração de método toString

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,3 +1,16 @@
 public class Gerente extends FuncionarioPJ {
     private Double valorBonidficacao;
+
+    @Override
+    public String toString() {
+        return "Gerente{" +
+                "valorBonidficacao=" + valorBonidficacao +
+                "nome='" + super.getNome() + '\'' +
+                ", documento='" + super.getEndereco().getBairro() + '\'' +
+                ", endereco=" + super.getEndereco() +
+                ", horasTrabalhadas=" + super.getHorasTrabalhadas() +
+                ", valorHora=" + super.getValorHora() +
+                ", valorRemuneracao=" + super.getValorRemuneracao() +
+                '}';
+    }
 }
